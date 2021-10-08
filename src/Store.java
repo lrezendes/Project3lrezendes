d9ae55236c93518e08da71360f762e17ec4e8031
jsantore <jsantore@bridgew.edu>
2021-10-08 15:09:06 -0400

    Final solution
    several of the warnings are pushing us towards either more advanced or more modern java shortcuts that we haven't discussed yet. I played around with a few of these, but I don't want to put too many of them in since some of you are planning to look this over to get over the difficulties you had in one or another small parts of the program

--- src/Store.java
+++ src/Store.java
@@ -25,7 +25,7 @@ public class Store {
   public Store () {
     Orders = new ArrayList<Order>();
     Customers = new ArrayList<Customer>();
-  };
+  }
   
   //
   // Methods
@@ -33,9 +33,7 @@ public class Store {
 
 
 
-  /**
-   * @param        args
-   */
+
   public static void main(String[] args) throws IOException
   {
     var comp152Inc = new Store();
@@ -63,6 +61,7 @@ public class Store {
           break;
         case 3:
           System.exit(0);
+
         default:
           System.out.println("\n%%%%%%Invalid selection, please choose one of the options from the menu%%%%%%\n");
       }
@@ -91,7 +90,6 @@ public class Store {
       fullPathName = Paths.get(filename);
       if (!Files.exists(fullPathName)){ //these three lines checks to see if the file exists, if not go
         System.out.println("No file with that name, please try again....");//do the loop again
-        continue;
       }
       else
         break;
@@ -113,6 +111,9 @@ public class Store {
    */
   public void makeOrder(ShippingAddress address, Customer cust)
   {
+    var newOrder = new Order(address,cust);
+    Orders.add(newOrder);
+    System.out.println(".......New order successfully created");
   }
 
 
@@ -158,6 +159,79 @@ public class Store {
    */
   public void manageCustomer(Customer selectedCustomer)
   {
+    //many of you passed our existing scanner in as an extra parameter, that is more than fine.
+    //I'm doing it this way to create a new one, both work the same way since both are reading from
+    //System.in
+    Scanner secondScanner = new Scanner(System.in);
+    while(true){ //the menu loop for the manage Customer menu
+        printCustomerMenu(selectedCustomer.getName());
+        var userChoice = secondScanner.nextInt();
+      //the syntax below is part of the 'enhanced switch' available in very recent versions of java
+      //I used the traditional version in the previous switch above, so I'm experimenting here
+      //this 'enhanced switch' doesn't require break statements.
+        switch (userChoice){
+          case 1 ->addAddress(secondScanner, selectedCustomer);
+          case 2->{
+            ShippingAddress selectedAddress = pickAddress(secondScanner,selectedCustomer);
+            makeOrder(selectedAddress,selectedCustomer);
+          }
+          case 3-> {return;}
+          default->System.out.println("Invalid option selected");
+        }
+    }
+  }
+
+  private ShippingAddress pickAddress(Scanner secondScanner, Customer selectedCustomer) {
+    var customerAddresses = selectedCustomer.getAddresses();
+    if (customerAddresses.size() ==0){ //note, this error checking was not required
+      System.out.println("This customer has no addresses on file, please add an address");
+      addAddress(secondScanner,selectedCustomer);
+      return selectedCustomer.getAddresses().get(0); //if we are here there is only one address so use it
+    }
+    var count = 0;
+    //if we are here then there was at least one address already in the customer.
+    System.out.println("Please select a shipping address from those the customer has on file");
+    for (var address : customerAddresses) {   //I'm being a little 'cute'/clever here
+      System.out.print("[" + count + "]"); //but you could do for(int count; count < customerAddresses.size(); count++ for the same effect
+      System.out.println(address.toString());
+      count++;
+    }
+    System.out.print("Enter the number of the address for this order:");
+    var addressNum = secondScanner.nextInt();
+    //again more error checking that I didn't require of you here below:
+    if (addressNum >= customerAddresses.size()){
+      System.out.println("Invalid entry, defaulting to the first address on file...");
+      return customerAddresses.get(0);
+    }
+    else
+      return customerAddresses.get(addressNum);//I asked the user for the number representing the addresses position in the arrayList
+  }
+
+  private void addAddress(Scanner secondScanner, Customer selectedCustomer) {
+    System.out.println("Adding new address for "+ selectedCustomer.getName());
+    secondScanner.nextLine(); //lets eat the leftover '\n' from previous nextInt
+    System.out.print("Enter Address Line 1:");
+    var line1 = secondScanner.nextLine();
+    System.out.print("Enter Address Line 2 or <enter> if there is none:");
+    var line2 = secondScanner.nextLine();
+    System.out.print("Enter the address City:");
+    var city = secondScanner.nextLine();
+    System.out.print("Enter address state:");
+    var state = secondScanner.nextLine();
+    System.out.print("Enter the postal code:");
+    var postCode = secondScanner.nextLine();
+    var newAddress  = new ShippingAddress(line1,line2,city,state,postCode);
+    selectedCustomer.addAddress(newAddress);
+  }
+
+  private void printCustomerMenu(String custName) {
+    System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    System.out.println("What do you want to do for Customer " + custName+"?");
+    System.out.println("   [1] Add Address to customer");
+    System.out.println("   [2] Make an order for the customer");
+    System.out.println("   [3] return to the main menu");
+    System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    System.out.print("Enter the number of your choice:");
   }
 
 
